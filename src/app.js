c8cfc5216831f7bf28d4f4c9c13bf7588ebb5dd6
dnssd <dnssdvlp@gmail.com>
2019-04-11 00:19:26 -0400

    Minors changes on person sections

--- src/app.js
+++ src/app.js
@@ -2,5 +2,9 @@ import "./styles/main.scss";
 import "./static/img/senaletica.jpg";
 
 
-console.log('first mssge :)');
+var ancho = screen.width;
+console.log(ancho);
 
+if(ancho < 1366){
+    console.log('ocultar');
+}
